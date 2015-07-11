e878a30a4fc26c3b0d9dda5c622a7bfbc760382b
Kwami Agbodji <verb0ze@gmail.com>
2015-07-11 12:42:54 -0400

    implement search menu in fragment

--- app/src/main/java/com/vbz/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/vbz/spotifystreamer/MainActivity.java
@@ -1,92 +1,32 @@
 package com.vbz.spotifystreamer;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private final String[] data = {"abdac", "dadsefg", "hijkik", "orujf", "dddd"};
-    private static final String LOG_TAG = "SPOTSTREAMER";
-
-    private void handleIntent(Intent intent) {
-        // TODO: this should be done in background thread
-        Log.d(LOG_TAG, "searched called");
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            //dummy data for testing
-            // ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-            // TODO: get real data from spotify wrapper here
-            List<String> found = new ArrayList<String>();
-            for (String s : data) {
-                if (s.indexOf(query) != 1) {
-                    found.add(s);
-                }
-            }
-            // ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-            if (found.size() > 0) {
-                // list data found
-                //TODO: pass arraylist to to adapter for display
-                //String[] res = found.toArray(new String[found.size()]);
-                Log.d(LOG_TAG, "data found: " + found.toString());
-                Toast.makeText(this, "found: " + found.toString(), Toast.LENGTH_SHORT).show();
-            } else {
-                // toast no data found
-                Log.e(LOG_TAG, "no data found");
-                Toast.makeText(this, "no data found for [" + query + "]", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainActivityFragment())
+                    .add(R.id.maincontainer, new MainActivityFragment())
                     .commit();
         }
-        Log.d(LOG_TAG, "handling search in onCreate");
-        handleIntent(getIntent());
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        Log.d(LOG_TAG, "handling search in onNewIntent");
-        handleIntent(intent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.artist_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
--- app/src/main/java/com/vbz/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/vbz/spotifystreamer/MainActivityFragment.java
@@ -1,30 +1,106 @@
 package com.vbz.spotifystreamer;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MainActivityFragment extends Fragment {
-    protected static ArrayAdapter<String> data;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivityFragment extends ListFragment {
+    private final String[] data = {"elisabeth", "nanako", "kate", "adama", "kerri"};
+    private static final String LOG_TAG = "SPOTSTREAMER";
+
+    private void search(String query) {
+        // TODO: this should be done in background thread
+        if (! TextUtils.isEmpty(query)) {
+            Log.d(LOG_TAG, "(fragment) searched called");
+            //dummy data for testing
+            // ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            // TODO: get real data from spotify wrapper here
+            List<String> found = new ArrayList<String>();
+            for (String s : data) {
+                if (s.contains(query)) {
+                    found.add(s);
+                }
+            }
+            // ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+            if (found.size() > 0) {
+                // list data found
+                //TODO: pass arraylist to to adapter for display
+                Log.d(LOG_TAG, "(fragment) data found: " + found.toString());
+                Toast.makeText(getActivity(), " fragment) data found: " + found.toString(), Toast.LENGTH_SHORT).show();
+            } else {
+                // toast no data found
+                Log.d(LOG_TAG, "(fragment) no data found");
+                Toast.makeText(getActivity(), "(fragment) no data found for [" + query + "]", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Log.d(LOG_TAG, "(fragment) empty search query");
+        }
+    }
 
     public MainActivityFragment() {
     }
 
-    @Override
-    public void onCreate(Bundle savedinstanceSate) {
+    @Override public void onCreate(Bundle savedinstanceSate) {
         super.onCreate(savedinstanceSate);
-//       setHasOptionsMenu(true);
+        setHasOptionsMenu(true);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                       Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // let the fragment resolve searches
+        // we get the theme for this activity so that text is rendered properly
+        Context themectx = ((AppCompatActivity) getActivity()).getSupportActionBar().getThemedContext();
+        SearchView sv = new SearchView(themectx);
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override public boolean onQueryTextSubmit(String query) {
+                Log.d(LOG_TAG, "submitting query: " + query);
+                search(query);
+                return true;
+            }
+
+            @Override public boolean onQueryTextChange(String newText) {
+                Log.d(LOG_TAG, "listening for... " + newText);
+                return true;
+            }
+        });
+        sv.setQueryHint("Search for an artist...");
+        sv.setOnCloseListener(null);
+        sv.setIconifiedByDefault(true);
+
+        // add a search icon to action bar
+        inflater.inflate(R.menu.menu_mainfragment, menu);
+        MenuItem searchview = menu.findItem(R.id.search);
+        searchview.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        searchview.setActionView(sv);
+    }
+
+    @Override public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        setEmptyText(getString(R.string.search_hint));
+    }
+
+    @Override public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO: display popular tracks for artist
+        Log.d(LOG_TAG, "Item clicked: " + id);
     }
 }
